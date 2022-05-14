7cc8b4a7685a3c454f987e78ff2a3c21c46276f6
toyama1710 <toyama@toyama1710.net>
2022-05-14 13:26:22 +0900

    fix

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 DIR=$PWD
 dconf load /com/gexperts/Tilix/ < tilix.conf
+
+rm -rf ~/.config/i3
 ln -snfv $DIR/i3 ~/.config/i3
+
+rm -rf ~/.config/i3blocks
 ln -snfv $DIR/i3blocks ~/.config/i3blocks
+
+rm -rf ~/.config/fish
 ln -snfv $DIR/fish ~/.config/fish
