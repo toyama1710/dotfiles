f29453b1fe4e055600b27c1234396a1fd93b6130
toyama <toyama@toyama1710.net>
2025-12-20 01:05:04 +0900

    remove fish

--- install.sh
+++ install.sh
@@ -9,7 +9,4 @@ ln -snfv $DIR/i3 ~/.config/i3
 rm -rf ~/.config/i3blocks
 ln -snfv $DIR/i3blocks ~/.config/i3blocks
 
-rm -rf ~/.config/fish
-ln -snfv $DIR/fish ~/.config/fish
-
 ln -snfv $DIR/vimrc ~/.vimrc
